91fcecfb66dcd5e1593f8d1f9d663f44ef1060e2
Jameel-Ahmed-arch <jameel41274@gmail.com>
2025-03-06 19:35:51 +0500

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -5,3 +5,8 @@ int main() {
     cout << "Jameel Ahmad-24-ST-37" << endl;
     return 0;
 }
+int a = 5, b = 3;
+if (b != 0)
+    cout << "Quotient: " << (a / b) << endl;
+else
+    cout << "Error: Division by zero" << endl;
